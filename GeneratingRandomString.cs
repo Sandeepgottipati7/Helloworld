69366e2c067dfdf57b3b9da3c4fef5fd7e335e2f
Sandeepgottipati7 <57784427+Sandeepgottipati7@users.noreply.github.com>
2019-11-15 15:23:18 +0530

    Create GeneratingRandomString.cs
    this is the code to generate random strings of a particular length

---
+++ GeneratingRandomString.cs
@@ -0,0 +1,19 @@
+ class Program
+    {
+      
+        
+        static void Main(string[] args)
+        {
+            var chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+            var stringChars = new char[6];
+            Random random = new Random();
+
+            for (int i = 0; i < stringChars.Length; i++)
+            {
+                stringChars[i] = chars[random.Next(chars.Length)];
+            }
+
+            var finalString = new String(stringChars);
+            Console.WriteLine(finalString);
+        }
+    }
